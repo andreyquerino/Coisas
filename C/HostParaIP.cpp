9bc9a4fa8a706e324bd6a19e94fc9735dd093e6f
Andrey <88719652+andreyquerino@users.noreply.github.com>
2021-08-10 13:14:30 -0300

    Update HostParaIP.cpp

--- C/HostParaIP.cpp
+++ C/HostParaIP.cpp
@@ -3,7 +3,7 @@
 // Compilação linux g++/i686-w64-mingw32-g++ <filename>.cpp -o <filname>.exe -lws2_32 -s -ffunction-sections -fdata-sections -Wno-write-strings -fno-exceptions -fmerge-all-constants -static-libstdc++ -static-libgcc
 // Compilação windows g++ -std=c++11 a.cpp -o a.exe -s -lws2_32 -Wno-write-strings -fno-exceptions -fmerge-all-constants -static-libstdc++ -static-libgcc
 //
-// Simples converção de nome host para Ip
+// Simples conversão de nome host para Ip
 
 #include <stdio.h>
 #include <winsock2.h>
